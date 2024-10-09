e39465396e44851cc25e41d0aaaee8a914f27b9d
lgregori <lgregori@LES-5CG4293116.corp.capgemini.com>
2024-10-09 09:05:39 +0200

    Created Dao interface extending JPARepository

--- src/main/java/com/capgemini/capsteam/repository/GameDao.java
+++ src/main/java/com/capgemini/capsteam/repository/GameDao.java
@@ -1,5 +1,12 @@
 package com.capgemini.capsteam.repository;
 
-public interface GameDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.capsteam.model.Game;
+
+
+@Repository
+public interface GameDao extends JpaRepository<Game, Integer>{
 
 }
