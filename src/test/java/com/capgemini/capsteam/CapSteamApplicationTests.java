6e958620c53f1db2a180ef763a0a778a56a1bfe7
guillerg <guillerg@LES-5CG12150S3.corp.capgemini.com>
2024-10-09 13:29:52 +0200

    prueba

--- src/test/java/com/capgemini/capsteam/CapSteamApplicationTests.java
+++ src/test/java/com/capgemini/capsteam/CapSteamApplicationTests.java
@@ -1,64 +1,28 @@
 package com.capgemini.capsteam;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.capgemini.capsteam.controller.GameController;
 import com.capgemini.capsteam.model.Game;
 import com.capgemini.capsteam.repository.GameDaoImpl;
 
 @SpringBootTest
 class CapSteamApplicationTests {
 	private GameDaoImpl GameDao;
-	@Before
-	 public void setUp() {
-        GameDao = new GameDaoImpl();
-    }
-	@Test
-    public void testSaveGame_successful() {
-        Game game = new Game();
-        game.setName("WWE 2k24");
-        game.setPlatform("Ps5");
-        game.setReleaseYear("2024");
-        game.setGenre("Fighting");
-        game.setDeveloper("Sony");
-        game.setNaSales("11.85");
-        game.setEuSales("24.88");
-        game.setJpSales("25.79");
-        game.setOtherSales("1.31");
-        game.setGlobalSales("63.83");
-        
-        boolean result = GameDao.saveGame(game);
-        assertTrue(result);
-    }
-//TEST NO EXISTE UN PARAMETRO
-    @Test
-    public void testSaveGame_missingParameters() {
-        Game game = new Game();
-        game.setName(""); 
-        game.setPlatform("Wii");
-        game.setReleaseYear("2008");
-        game.setGenre("Racing");
-        game.setDeveloper("Nintendo");
-        game.setNaSales("15.85");
-        game.setEuSales("12.88");
-        game.setJpSales("3.79");
-        game.setOtherSales("3.31");
-        game.setGlobalSales("35.82");
-        boolean result = GameDao.saveGame(game);
-        assertFalse(result); 
-    }
-//FECHA FORMATO INCORRECTO
-    @Test
-    public void testSaveGame_invalidDate() {
-        Game game = new Game();
-        game.setName("Test Game");
-        game.setDeveloper("Test Developer");
-        game.setReleaseDate("09-10-2030"); // Fecha en formato incorrecto
+	
+	
 
-        boolean result = GameDao.saveGame(game);
-        assertFalse(result); // Debería fallar por un formato de fecha incorrecto
-    }
-	}
 
+@Autowired
+private GameController controller;
+
+@Test
+void shouldExistsController() {
+	assertThat(controller).isNotNull();
+}
 }
