025be544fe4374121fde565eb0a618651df7a053
ecantone <ecantone@LES-PF4PZWF2.corp.capgemini.com>
2024-10-09 12:17:37 +0200

    Created list html and listGames service

--- src/main/java/com/capgemini/capsteam/controller/GameController.java
+++ src/main/java/com/capgemini/capsteam/controller/GameController.java
@@ -1,5 +1,28 @@
 package com.capgemini.capsteam.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.capgemini.capsteam.service.GameService;
+
+@Controller
 public class GameController {
+	@Autowired
+	GameService service;
+	
+//private static final Logger log = LoggerFactory.getLogger(GameController.class);
+	
+	//Listar games
+	@GetMapping("/")
+	public String listGames(Model m) {
+		m.addAttribute("gameList",service.findAll());
+		
+		return "GameList.html";
+	}
+	
 
 }
--- src/main/java/com/capgemini/capsteam/service/GameService.java
+++ src/main/java/com/capgemini/capsteam/service/GameService.java
@@ -1,5 +1,12 @@
 package com.capgemini.capsteam.service;
 
+import java.util.List;
+
+import com.capgemini.capsteam.model.Game;
+
 public interface GameService {
+	
+	//Metodo para obtener la lista de juegos
+	public List<Game> findAll();
 
 }
