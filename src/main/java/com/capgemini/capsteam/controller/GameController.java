dcdf1c71a721a99703c45d8efb1d4df8c72d19e5
Parra Graca <veronica.parra-graca@capgemini.com>
2024-10-09 10:42:17 +0200

    getGameByName()implemented on controller

--- src/main/java/com/capgemini/capsteam/controller/GameController.java
+++ src/main/java/com/capgemini/capsteam/controller/GameController.java
@@ -1,5 +1,30 @@
 package com.capgemini.capsteam.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.capgemini.capsteam.model.Game;
+import com.capgemini.capsteam.service.GameService;
+
+@Controller
+@RequestMapping("/games")
 public class GameController {
+	
+	@Autowired
+	GameService service;
+
+	  @GetMapping("/search")
+	    public List<Game> searchGameByName(@RequestParam String name) {
+	        return service.getGameByName(name);
+	    }
+	
+	
+	
+
 
 }
