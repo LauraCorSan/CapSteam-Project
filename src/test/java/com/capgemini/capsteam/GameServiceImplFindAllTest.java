a9bb207f9628554cde3b3352c6ebe178d69bc8d2
ecantone <ecantone@LES-PF4PZWF2.corp.capgemini.com>
2024-10-09 16:55:16 +0200

    update documentation

--- src/test/java/com/capgemini/capsteam/GameServiceImplFindAllTest.java
+++ src/test/java/com/capgemini/capsteam/GameServiceImplFindAllTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)  //línea para habilitar la extensión de Mockito, si no da error
-class GameServiceTest {
+class GameServiceImplFindAllTest {
 
     @Mock
     private GameDao gameDao;  //mockeamos el repositorio
@@ -34,6 +34,13 @@ class GameServiceTest {
         game2 = new Game(2, "Game2", "Platform2", 2021, "Genre2", "Publisher2", 2.0, 1.2, 0.8, 0.4, 4.4);
     }
 
+    
+    /**
+     * Prueba unitaria para verificar el comportamiento del método findAll() 
+     * del servicio GameServiceImpl. Simula la respuesta del repositorio 
+     * (GameDao) utilizando Mockito y valida que la lista de juegos 
+     * devuelta contenga los juegos esperados.
+     */
     @Test
     void testFindAll() {
         //simulamos la respuesta del repositorio
