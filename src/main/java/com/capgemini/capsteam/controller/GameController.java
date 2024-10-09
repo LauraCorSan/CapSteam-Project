f496009337a01eb13921cd6eb99616f5f1d2b580
guillerg <guillerg@LES-5CG12150S3.corp.capgemini.com>
2024-10-09 13:30:08 +0200

    prueba5

--- src/main/java/com/capgemini/capsteam/controller/GameController.java
+++ src/main/java/com/capgemini/capsteam/controller/GameController.java
@@ -3,26 +3,30 @@ package com.capgemini.capsteam.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.capgemini.capsteam.service.GameService;
+import com.capgemini.capsteam.model.Game;
 
-@Controller
+import ch.qos.logback.core.model.Model;
+
 public class GameController {
 	@Autowired
-	GameService service;
+	Game Service service;
 	
-//private static final Logger log = LoggerFactory.getLogger(GameController.class);
+	private static final Logger log = LoggerFactory.getLogger(GameController.class);
 	
-	//Listar games
-	@GetMapping("/")
-	public String listGames(Model m) {
-		m.addAttribute("gameList",service.findAll());
+	//Nuevo Juego
+		@GetMapping("/new")
+		public String newUser(Game game, Model g) {
+			g.addAttribute("game",game);
+			return "GameForm";
+		}
 		
-		return "GameList.html";
-	}
-	
-
+		//Guardar Usuario
+		@PostMapping("/save")
+		public String saveUser(Game game) {
+			Service.save(game);
+			return ("redirect:/");
+		}
 }
