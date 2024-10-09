58a0f9c80ebd1c5089e444e8f1184ab33ed2d51c
lcordero <lcordero@LES-5CG4293110.corp.capgemini.com>
2024-10-09 12:18:16 +0200

    Create delete method in controller layer

--- src/main/java/com/capgemini/capsteam/controller/GameController.java
+++ src/main/java/com/capgemini/capsteam/controller/GameController.java
@@ -1,5 +1,27 @@
 package com.capgemini.capsteam.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.capgemini.capsteam.service.GameService;
+
+@Controller
+@RequestMapping("/game")
 public class GameController {
 
+	@Autowired
+	GameService service;
+
+	private static final Logger log = LoggerFactory.getLogger(GameController.class);
+
+	@GetMapping("/delete")
+	public String deleteGame(@RequestParam("rank") int rank) {
+		service.deleteById(rank);
+		return ("redirect:/");
+	}
 }
